3600678ccf129606eef7fff7ad99331b2236b8be
BroD-Prod <brodyroth23@gmail.com>
2025-05-07 16:41:35 -0400

    Initial Commit

---
+++ password.py
@@ -0,0 +1,10 @@
+def writePassword():
+    possible_chars = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,1,2,3,4,5,6,7,8,9,0"
+    passwordAmount = input("Input the amount of desired Unique Strong Passwords: ")
+    for i in passwordAmount:
+        k = possible_chars.split(",")
+        print(k)
+
+
+
+writePassword()

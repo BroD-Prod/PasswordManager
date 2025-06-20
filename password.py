86bf54f687309afaa40ab4f2bc5463b6cc53ab5b
BroD-Prod <brodyroth23@gmail.com>
2025-06-19 20:29:10 -0400

    Adding user creation and database implementation, as well as adding user registration and login functionality.
    This is a very basic outline of what the app will do. Users can now have the ability to create themselves as a user, and be secured.

--- password.py
+++ password.py
@@ -1,5 +1,5 @@
 import hashlib
-
+import pymongo
 
 class Password:
     def __init__(self):
@@ -12,15 +12,47 @@ class Password:
         return hashed_password
 
 
-def main():
+def register_user():
+    client = pymongo.MongoClient()
+    my_db = client["password_manager"]
+    collection = my_db["users"]
+    password_manager = Password()
     username = input("Please create a username: ")
     password = input("Please create a Password: ")
-    password_manager = Password()
     hashed_password = password_manager.hash_password(password)
-    user = {
-        username : hashed_password
+    user_dict = {
+       "username": username,
+        "hashed_password": hashed_password
     }
-    print(user)
+    collection.insert_one(user_dict)
+
+def login_user():
+    client = pymongo.MongoClient()
+    my_db = client["password_manager"]
+    collection = my_db["users"]
+    password_manager = Password()
+    username = input("Please enter your username: ")
+    password = input("Please enter your password: ")
+
+    if not collection.find({"username": username}):
+        print("Username Not Found, Please Try Again")
+        return
+    if not collection.find({"hashed_password": password_manager.hash_password(password)}):
+        print("Password Invalid")
+    else:
+        print("Welcome to Password Manager")
+
+def main():
+    client = pymongo.MongoClient()
+    my_db = client["password_manager"]
+    collection = my_db["users"]
+    while True:
+        user_status = input("Welcome to Password Manager, Are You a New User? (Y/N)")
+        if user_status == "Y":
+           register_user()
+        else:
+            login_user()
+
 
 if __name__ == "__main__":
     main()
